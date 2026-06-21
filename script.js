1a00e6ee87f98f19f621a84b9c815b60f8cbb607
nithyasanthoshi876 <snithyasanthoshi@gmail.com>
2026-06-21 09:41:31 +0530

    full screen hearts

--- script.js
+++ script.js
@@ -27,25 +27,30 @@ window.onload = function () {
 };
 
 function createPetals() {
-    const petals = document.getElementById("petals");
-
     setInterval(() => {
 
         const petal = document.createElement("div");
 
-        petal.className = "petal";
-        petal.innerHTML = Math.random() > 0.5 ? "🌸" : "❤️";
+        petal.innerHTML = Math.random() > 0.5 ? "❤️" : "🌸";
 
-        petal.style.left = Math.random() * 100 + "vw";
-        petal.style.fontSize = (20 + Math.random() * 20) + "px";
-        petal.style.animationDuration =
-            (5 + Math.random() * 5) + "s";
+        petal.style.position = "fixed";
+        petal.style.left = Math.random() * window.innerWidth + "px";
+        petal.style.top = "-50px";
 
-        petals.appendChild(petal);
+        petal.style.fontSize =
+            (20 + Math.random() * 25) + "px";
+
+        petal.style.zIndex = "9999";
+        petal.style.pointerEvents = "none";
+
+        petal.style.animation =
+            "fall " + (5 + Math.random() * 5) + "s linear forwards";
+
+        document.body.appendChild(petal);
 
         setTimeout(() => {
             petal.remove();
         }, 10000);
 
-    }, 300);
+    }, 200);
 }
